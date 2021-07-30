f626a51a8458309423fbb8457bccf2bc2bd87d88
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-07-30 06:55:28 +0400

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void minBox_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
